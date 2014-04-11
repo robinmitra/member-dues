3ff2523ad7ee724a04db0e6c8578df6f8b772279
Robin Mitra <robinmitra1@gmail.com>
2014-04-11 16:45:46 +0100

    Refactor and cleanup.

--- CRM/Memberdues/Form/MemberDues.php
+++ CRM/Memberdues/Form/MemberDues.php
@@ -11,67 +11,29 @@ class CRM_Memberdues_Form_MemberDues extends CRM_Core_Form
 {
   function buildQuickForm()
   {
+    $this->addFormFields();
+    $this->addTotalMembershipDues();
+    parent::buildQuickForm();
+  }
+
+  /**
+   * Add form fields to the template
+   */
+  protected function addFormFields()
+  {
     $this->add(
       'date',
       'start_date',
       'Start Date',
-      array('minYear' => 2000, 'maxYear' => 2014, 'addEmptyOption'   => true)
+      array('minYear' => 2000, 'maxYear' => 2014, 'addEmptyOption' => true)
     );
 
     $this->add(
       'date',
       'end_date',
       'End Date',
-      array('minYear' => 2000, 'maxYear' => 2014, 'addEmptyOption'   => true)
+      array('minYear' => 2000, 'maxYear' => 2014, 'addEmptyOption' => true)
     );
-
-    $values = $this->exportValues();
-
-    $contactId = $this->getContactID();
-    $financialTypeId = 2;
-
-    if (array_key_exists('start_date', $values)) {
-      $startDate = $this->getMysqlDate($values['start_date']);
-    } else {
-      $dateTime = new DateTime();
-      $dateTime->modify('-1 year');
-      $startDate = $dateTime->format('Y-m-d 00:00:00');
-    }
-
-    if (array_key_exists('end_date', $values)) {
-      $endDate = $this->getMysqlDate($values['end_date']);
-    } else {
-      $dateTime = new DateTime();
-      $endDate = $dateTime->format('Y-m-d 00:00:00');
-    }
-
-    $sql = "
-		SELECT SUM(total_amount) AS total
-		FROM civicrm_contribution
-		WHERE contact_id = %0
-		AND financial_type_id = %1
-		AND receive_date > %2
-		AND receive_date < %3
-	";
-
-    $params = array(
-      array($contactId, 'Int'),
-      array($financialTypeId, 'Int'),
-      array($startDate, 'String'),
-      array($endDate, 'String')
-    );
-
-	  $queryStr = CRM_Core_DAO::composeQuery($sql, $params, true);
-
-    /** @var CRM_Core_DAO $result */
-    $result = CRM_Core_DAO::executeQuery($sql, $params);
-
-    /** @var DB_result $dbResult */
-    $dbResult = $result->getDatabaseResult();
-
-    /** @var int $total Total contribution amount */
-    $total = (int) reset($dbResult->fetchRow());
-    CRM_Core_Smarty::singleton()->appendValue('total', $total);
 
     $this->addButtons(array(
       array(
@@ -83,18 +45,101 @@ class CRM_Memberdues_Form_MemberDues extends CRM_Core_Form
 
     // export form elements
     $this->assign('elementNames', $this->getRenderableElementNames());
-    parent::buildQuickForm();
   }
 
-  protected function getMysqlDate(array $dateArr)
+  /**
+   * Add total amount of membership dues to the template
+   */
+  protected function addTotalMembershipDues()
   {
-    $day = str_pad($dateArr['d'], 2, '0', STR_PAD_LEFT);
-    $month = str_pad($dateArr['M'], 2, '0', STR_PAD_LEFT);
-    $year = $dateArr['Y'];
+    $total = $this->getTotalMembershipDues();
+    $this->assign('total', $total);
+  }
 
-    $date = $year . '-' . $month . '-' . $day . ' 00:00:00';
+  /**
+   * Return total amount of membership dues for the current contact
+   *
+   * @return int
+   */
+  protected function getTotalMembershipDues()
+  {
+    $values = $this->exportValues();
 
-    return $date;
+    $contactId = $this->getContactID();
+
+    /** @var int $financialTypeId Value of 2 refers to 'Membership dues' */
+    $financialTypeId = 2;
+
+    $startDate = $this->getStartDate($values);
+    $endDate = $this->getEndDate($values);
+
+    $sql = "
+      SELECT SUM(total_amount) AS total
+      FROM civicrm_contribution
+      WHERE contact_id = %0
+      AND financial_type_id = %1
+      AND receive_date > %2
+      AND receive_date < %3
+	  ";
+
+    $params = array(
+      array($contactId, 'Int'),
+      array($financialTypeId, 'Int'),
+      array($startDate, 'String'),
+      array($endDate, 'String')
+    );
+
+    // $queryStr = CRM_Core_DAO::composeQuery($sql, $params, true);
+
+    /** @var CRM_Core_DAO $result */
+    $result = CRM_Core_DAO::executeQuery($sql, $params);
+
+    /** @var DB_result $dbResult */
+    $dbResult = $result->getDatabaseResult();
+
+    /** @var int $total Total contribution amount */
+    $total = (int)reset($dbResult->fetchRow());
+    return $total;
+  }
+
+  /**
+   * Return end date in standard MySQL format
+   *
+   * @param $values
+   * @return string
+   */
+  protected function getEndDate($values)
+  {
+    if (array_key_exists('end_date', $values)) {
+      $endDate = $this->getMysqlDate($values['end_date']);
+      return $endDate;
+    } else {
+      $dateTime = new DateTime();
+      $endDate = $dateTime->format('Y-m-d 00:00:00');
+      return $endDate;
+    }
+  }
+
+  /**
+   * Return start date in standard MySQL format
+   *
+   * @param array $values
+   *
+   * @return string
+   */
+  protected function getStartDate(array $values)
+  {
+    $startDate = null;
+
+    if (array_key_exists('start_date', $values)) {
+      $startDate = $this->getMysqlDate($values['start_date']);
+    } else {
+      $dateTime = new DateTime();
+      $dateTime->modify('-1 year');
+      $startDate = $dateTime->format('Y-m-d 00:00:00');
+    }
+
+    return $startDate;
   }
 
   /**
@@ -116,5 +161,23 @@ class CRM_Memberdues_Form_MemberDues extends CRM_Core_Form
       }
     }
     return $elementNames;
+  }
+
+  /**
+   * Get date in standard MySQL format
+   *
+   * @param array $dateArr
+   *
+   * @return string
+   */
+  protected function getMysqlDate(array $dateArr)
+  {
+    $day = str_pad($dateArr['d'], 2, '0', STR_PAD_LEFT);
+    $month = str_pad($dateArr['M'], 2, '0', STR_PAD_LEFT);
+    $year = $dateArr['Y'];
+
+    $date = $year . '-' . $month . '-' . $day . ' 00:00:00';
+
+    return $date;
   }
 }
--- templates/CRM/Memberdues/Form/MemberDues.tpl
+++ templates/CRM/Memberdues/Form/MemberDues.tpl
@@ -3,19 +3,9 @@
 {include file="CRM/common/formButtons.tpl" location="top"}
 </div>
 
-{* FIELD EXAMPLE: OPTION 1 (AUTOMATIC LAYOUT) *}
-
-{*foreach from=$elementNames item=elementName}
-  <div class="crm-section">
-    <div class="label">{$form.$elementName.label}</div>
-    <div class="content">{$form.$elementName.html}</div>
-    <div class="clear"></div>
-  </div>
-{/foreach*}
-
 <div class="crm-section">
     <div class="label">
-        <label for="">{$form.start_date.label}</label>
+        <label>{$form.start_date.label}</label>
     </div>
     <div class="content">
         {$form.start_date.html}
@@ -24,7 +14,7 @@
 </div>
 <div class="crm-section">
     <div class="label">
-        <label for="">{$form.end_date.label}</label>
+        <label>{$form.end_date.label}</label>
     </div>
     <div class="content">
         {$form.end_date.html}
@@ -32,21 +22,13 @@
     <div class="clear"></div>
 </div>
 
-{* FIELD EXAMPLE: OPTION 2 (MANUAL LAYOUT)
-
-  <div>
-    <span>{$form.favorite_color.label}</span>
-    <span>{$form.favorite_color.html}</span>
-  </div>
-
-{* FOOTER *}
 <div class="crm-submit-buttons">
 {include file="CRM/common/formButtons.tpl" location="bottom"}
 </div>
 
 <div class="crm-section">
     <div class="label">
-        <label for="">{$form.member_dues.label}</label>
+        <label>{$form.member_dues.label}</label>
     </div>
     <div class="content">
         {$form.member_dues.html}
@@ -55,3 +37,4 @@
 </div>
 
 <p>Total member dues: <strong>{$total}</strong></p>
+{* FOOTER *}
